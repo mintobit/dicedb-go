ac1533a050c7a1e1488750154e32286e683b9660
Arpit <arpit.b.bhayani@gmail.com>
2025-02-04 17:21:09 +0000

    Adding FireString utility

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/dicedb/dicedb-go
 go 1.23.5
 
 require google.golang.org/protobuf v1.36.4
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package dicedb
 import (
 	"fmt"
 	"net"
+	"strings"
 	"time"
 
 	"github.com/dicedb/dicedb-go/ironhawk"
 	"github.com/dicedb/dicedb-go/wire"
+	"github.com/google/uuid"
 )
 
 type Client struct {
-	conn net.Conn
+	id        string
+	conn      net.Conn
+	watchConn net.Conn
+	watchCh   chan *wire.Response
+	host      string
+	port      int
 }
+
+type Option func(*Client)
 
 func newConn(host string, port int) (net.Conn, error) {
 	addr := fmt.Sprintf("%s:%d", host, port)
@@ -22,12 +31,35 @@ func newConn(host string, port int) (net.Conn, error) {
 	return conn, nil
 }
 
-func NewClient(host string, port int) (*Client, error) {
+func WithID(id string) Option {
+	return func(c *Client) {
+		c.id = id
+	}
+}
+
+func NewClient(host string, port int, opts ...Option) (*Client, error) {
 	conn, err := newConn(host, port)
 	if err != nil {
 		return nil, err
 	}
-	return &Client{conn: conn}, nil
+
+	client := &Client{conn: conn, host: host, port: port}
+	for _, opt := range opts {
+		opt(client)
+	}
+
+	if client.id == "" {
+		client.id = uuid.New().String()
+	}
+
+	if resp := client.Fire(&wire.Command{
+		Cmd:  "CLIENT.ID",
+		Args: []string{client.id},
+	}); resp.Err != "" {
+		return nil, fmt.Errorf("could not complete the handshake: %s", resp.Err)
+	}
+
+	return client, nil
 }
 
 func (c *Client) Fire(cmd *wire.Command) *wire.Response {
@@ -45,6 +77,43 @@ func (c *Client) Fire(cmd *wire.Command) *wire.Response {
 	}
 
 	return resp
+}
+
+func (c *Client) FireString(cmdStr string) *wire.Response {
+	cmdStr = strings.TrimSpace(cmdStr)
+	tokens := strings.Split(cmdStr, " ")
+
+	var args []string
+	var cmd = tokens[0]
+	if len(tokens) > 1 {
+		args = tokens[1:]
+	}
+
+	return c.Fire(&wire.Command{
+		Cmd:  cmd,
+		Args: args,
+	})
+}
+
+func (c *Client) WatchCh() (<-chan *wire.Response, error) {
+	var err error
+	if c.watchCh != nil {
+		return c.watchCh, nil
+	}
+
+	c.watchCh = make(chan *wire.Response)
+	c.watchConn, err = newConn(c.host, c.port)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp := c.Fire(&wire.Command{
+		Cmd: "CLIENT.WATCH",
+	}); resp.Err != "" {
+		return nil, fmt.Errorf("could not watch at the moment: %s", resp.Err)
+	}
+
+	return c.watchCh, nil
 }
 
 func (c *Client) Close() {
